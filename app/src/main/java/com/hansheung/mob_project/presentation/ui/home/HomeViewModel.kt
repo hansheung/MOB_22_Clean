c0e966788ae7f568ba6caf253d9f4b1e543bddbd
Hansheung <hansheung@gmail.com>
2025-03-26 10:52:21 +0800

    MOB 22 Clean

---
+++ app/src/main/java/com/hansheung/mob_project/presentation/ui/home/HomeViewModel.kt
@@ -0,0 +1,12 @@
+package com.hansheung.mob_project.presentation.ui.home
+
+import com.hansheung.mob_project.presentation.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor (
+
+) : BaseViewModel() {
+    // TODO: Implement the ViewModel
+}
